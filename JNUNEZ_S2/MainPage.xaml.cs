25f545bbda60f3a28b76affcaeee9976392de6e4
jeffe <jeffe@LAPTOP-KHGKRI9S>
2021-04-30 23:35:48 -0500

    Agregar archivos de proyecto.

---
+++ JNUNEZ_S2/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace JNUNEZ_S2
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage(string usuario, string clave)
+        {
+            InitializeComponent();
+            txtusuario.Text = usuario;
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double dato1 = Convert.ToDouble(txtNota1.Text);
+                double dato2 = Convert.ToDouble(txtNota2.Text);
+                double dato3 = Convert.ToDouble(txtNota3.Text);
+                double dato4 = Convert.ToDouble(txtNota4.Text);
+                double nota1 = dato1 * 0.3;
+                double nota2 = dato2 * 0.2;
+                double nota3 = dato3 * 0.3;
+                double nota4 = dato4 * 0.2;
+                double parcial1 = nota1 + nota2;
+                double parcial2 = nota3 + nota4;
+                double total = parcial1 + parcial2;
+                if (total >= 7)
+                {
+                    txtEstado.Text = "APROBADO";
+                }
+                else
+                {
+                    txtEstado.Text = "REPROBADO";
+                }
+
+                txtNotaParcial1.Text = parcial1.ToString();
+                txtNotaParcial2.Text = parcial2.ToString();
+                txtNotaFinal.Text = total.ToString();
+
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", ex.Message, "Gracias");
+            }
+        }
+    }
+}
---
+++ JNUNEZ_S2/ventana.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JNUNEZ_S2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ventana : ContentPage
+    {
+        public ventana()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentana_Clicked(object sender, EventArgs e)
+        {
+            String usuario = txtUsuario.Text;
+            String clave = txtClave.Text;
+            if (usuario.Equals("estudiante2021") && clave.Equals("uisrael2021"))
+            {
+                await Navigation.PushAsync(new MainPage(usuario, clave));
+            }
+            else
+            {
+                await DisplayAlert("Alerta", "Clave o usuario incorrectos", "OK");
+            }
+        }
+    }
+}
